43d60ae85a31727265adf67d9d991f067a62522d
guiyi <zouhdai@gmail.com>
2017-03-22 15:18:12 +0800

    child process

---
+++ bb.py
@@ -0,0 +1,25 @@
+import MySQLdb;
+import os, string, sys,re,string;
+import time;
+global PATH,conn,cursor;
+
+LETTER="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+letter="abcdefghijklmnopqrstuvwxyz";
+os.chdir("/home/hongdai.zou/process.new.data/program/buyer");
+backupfile = "cp -f temp.buyer.file.preprocessed temp.file.b";
+os.system(backupfile);
+for i in range(13, 26) :
+    command1 =  'awk -F \"\t\" \'{print $2\"\t\"$1\"\t\"$3\", \"$4\", \" $5\", \"$6}\' yellow.page.and.info.USA.for.clustering.with.label |grep \"^'+LETTER[i]+'\" | awk -F \"\t\" \'{print $2\"\t\"$1\"\"$1\"\t\"$3}\' > temp.external.b'
+    print command1;
+    command2 =  'awk -F \"\t\" \'{print $2\"\t\"$1\"\t\"$3}\' temp.buyer.file.preprocessed |grep \"^'+letter[i]+'\" |awk -F \"\t\" \'{print  $2\"\t\"$1\"\t\"$3}\'> temp.real.data.b'
+    print command2;
+    command3 = "cat  temp.real.data.b  temp.external >clustering.ready."+LETTER[i]
+    print command3;
+    command4 = "python  buyer.clustering.py  -c clustering.del.reg.rules.USA -s 0.9 0.7 -f clustering.ready."+LETTER[i]+"  -o  temp.buyer.file.preprocessed.clustering.result."+LETTER[i]
+    print command4;
+    
+
+    os.system(command1);
+    os.system(command2);
+    os.system(command3);
+    os.system(command4);
